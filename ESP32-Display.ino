8c0ea50d415612787aa95b732c1f1f82246d209a
Valery Vasilyev <val@etuline.ru>
2022-05-19 23:35:49 +0300

    Добавлен тормоз (regenerate) и OLED экран Ver 0.13
    Не проверено ещё

--- ESP32-Display.ino
+++ ESP32-Display.ino
@@ -1,16 +1,24 @@
 #include <HardwareSerial.h>
 #include "BluetoothSerial.h"
 
+#include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
+// OR #include "SH1106Wire.h"   // legacy: #include "SH1106.h"
+
 #define RX_PIN 16
 #define TX_PIN 17
 #define TIMEOUT 20
+#define PWR_TIMEOUT 2000
 #define PROTO_1_BYTES 11
 #define PROTO_2_BYTES 8
-#define PROG_VER 0.12
+
 #define ACC_PIN 34
-#define ST_PIN 35
+#define REG_PIN 35
+#define POWER_PIN 23
+#define POWER_BT 19
 #define WHEEL_D 7.8
 #define MY_NAME "ESP32dsp"
+
+#define PROG_VER 0.13
 
 
 //bool start_p = false;
@@ -22,26 +30,60 @@ uint8_t BTmac[6] = {0};
 byte r_packet[] = {0x3e, 0x04, 0x01, 0x00, 0x00, 0x00, 0x00, 0x05};
 byte start_packet[] = {0x3e, 0x2c, 0x0b, 0x00, 0x3c, 0x28, 0x1e, 0x32, 0x46, 0x64, 0x01, 0x00, 0x00, 0x00, 0x01, 0x96};
 unsigned long loop_time;
+unsigned long pwr_bt_time;
 int16_t mk_speed, bat_current;
 uint16_t accValue = 0;
+uint16_t regValue = 0;
 char myBTName[22] = {0};
 unsigned long sensorValue;
 uint8_t Err_N = 0;
 
+uint16_t min_acc_value = 800;
+uint16_t max_acc_value = 3050;
+uint16_t min_reg_value = 800;
+uint16_t max_reg_value = 3050;
+
 boolean is_start_p = true;
 boolean is_error = false;
+boolean mk_ok = false;
+boolean pwr_bt = false;
+boolean Power_Off = false;
+boolean Power_On = true;
 
 HardwareSerial mySerial(2);
 BluetoothSerial SerialBT;
 
+// Initialize the OLED display using Arduino Wire:
+SSD1306Wire display(0x3c, SDA, SCL);   // ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically based on your board's pins_arduino.h e.g. https://github.com/esp8266/Arduino/blob/master/variants/nodemcu/pins_arduino.h
+// SSD1306Wire display(0x3c, D3, D5);  // ADDRESS, SDA, SCL  -  If not, they can be specified manually.
+// SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);  // ADDRESS, SDA, SCL, OLEDDISPLAY_GEOMETRY  -  Extra param required for 128x32 displays.
+// SH1106Wire display(0x3c, SDA, SCL);     // ADDRESS, SDA, SCL
+
+
+
 void setup() {
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(POWER_PIN, OUTPUT);
+  pinMode(POWER_BT, INPUT_PULLUP);
+  pinMode(ACC_PIN, INPUT_PULLUP);
+  pinMode(REG_PIN, INPUT_PULLUP);
+
+  digitalWrite(POWER_PIN,HIGH);
+
 
   esp_read_mac(BTmac, ESP_MAC_BT);
   sprintf(myBTName, "%s%02X%02X%02X%02X%02X%02X", MY_NAME, BTmac[0], BTmac[1], BTmac[2], BTmac[3], BTmac[4], BTmac[5]);
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(ACC_PIN, INPUT_PULLUP);
-  pinMode(ST_PIN, INPUT_PULLUP);
+
+ // Initialising the UI will init the display too.
+  display.init();
+
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_24);
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(0, 0, String(PROG_VER));
+  display.display();
 
   // initialize both serial ports:
   Serial.begin(115200);
@@ -82,13 +124,24 @@ void setup() {
   Serial.println("Port 9600 8N1 RX pin 16 TX pin 17");
   SerialBT.println("Port 9600 8N1 RX pin 16 TX pin 17");
 
-  accValue = readACC();
-  if(accValue != 0) {
+// accValue = readACC();
+//  if(accValue != 0) {
+  if(readACC() != 0) {
       Serial.printf("Значение акселератора: %d\n", accValue);
       Serial.println("Требуется калибровка!!!");
       is_error = true;
-      Err_N = 5;
+      Err_N = 5; // TODO:
   }
+
+//  regValue = readREG();
+//  if(regValue != 0) {
+  if(readREG() != 0) {
+      Serial.printf("Значение тормоза: %d\n", accValue);
+      Serial.println("Требуется калибровка!!!");
+      is_error = true;
+      Err_N = 6; // TODO:
+  }
+  
 
   calc_start_CRC(1);
 
@@ -97,7 +150,9 @@ void setup() {
 }
 
 void loop() {
-  
+
+  if(Power_Off) while(true);  // режим выключения TODO: небольшой таймаут с инфомацией на экране о выключении
+
   if (mySerial.available()) {
     oldByte = inByte;
     inByte = mySerial.read();
@@ -121,37 +176,41 @@ void loop() {
       if(packet[0] == 0x3c && packet[1] == 0x07) {
         printPacket();
         byte_p = 0;
+        mk_ok = true;
       }
     } else if (uart_protocol == 2 && byte_p == PROTO_2_BYTES) {
       if(packet[0] == 0x28) {
         printPacket();
         byte_p = 0;
+        mk_ok = true;
       }
     }
 
-    if(byte_p >= 29) {
+    if(byte_p >= 29) { // ошибка приема данных, не верный протокол?!
       uart_protocol = 0;
+      mk_ok = false;
       protoError();
-      byte_p = 0; // переполнение буфера!
+      byte_p = 0; 
     }
     
   }
 
-  tik();  
+  tik();
+
+  check_pwr_bts();
 
   check_serial();
-
-
 
 }
 
 void sendPacket() {
-  if(is_start_p) {
+  if(is_start_p&&mk_ok) {
     is_start_p = false;
     
     mySerial.write(start_packet,sizeof(start_packet));
   } else {
     r_packet[4] = readACC();
+    r_packet[5] = readREG();
 
     int16_t myCRC = r_packet[1] + r_packet[2] + r_packet[3] + r_packet[4] + r_packet[5];
     r_packet[6] = highByte(myCRC);
@@ -163,9 +222,9 @@ void sendPacket() {
 
 void protoError() {
   Serial.println();
-  SerialBT.println();
+//  SerialBT.println();
   Serial.print("Protocol unknown!!!");
-  SerialBT.println("Protocol unknown!!!");
+  SerialBT.println("PROTO ERR");
   
 }
 
@@ -206,6 +265,27 @@ void tik() {
   }
 }
 
+void check_pwr_bts() {
+
+  if(digitalRead(POWER_BT) == LOW) // кнопка питания нажата
+    if(!Power_On) { // режим включения ???
+      if(!pwr_bt) { // кнопку питания только нажали 
+        pwr_bt = true;
+        pwr_bt_time = millis() + PWR_TIMEOUT;
+      } else { // кнопка питания уже была нажата
+        if(millis() > pwr_bt_time) { // прошло 2 секунды для выключения ?
+          Power_Off = true;
+          digitalWrite(POWER_PIN, LOW);
+        }
+      }
+    }
+  else {
+    if(Power_On) Power_On = false;
+    if(pwr_bt) pwr_bt = false;
+  }
+}
+
+
 void check_serial() {
   if (Serial.available()) {
     cmdByte = Serial.read();
@@ -217,9 +297,9 @@ void check_serial() {
     if(cmdByte == '2') r_packet[2] = 0x02;  // переключаем скорость на 2
     if(cmdByte == '3') r_packet[2] = 0x03;  // переключаем скорость на 3
 
-    if(cmdByte == 'f' || cmdByte == 'F' && (r_packet[5] <= 200)) r_packet[5]++;
+//    if(cmdByte == 'f' || cmdByte == 'F' && (r_packet[5] <= 200)) r_packet[5]++;
 //    if(cmdByte == 'c' || cmdByte == 'C' && (r_packet[5] >= 1)) r_packet[5]--;
-    if(cmdByte == '-') r_packet[5] = 0x00;  // сбрасываем тормоз
+//    if(cmdByte == '-') r_packet[5] = 0x00;  // сбрасываем тормоз
 
     
     int16_t myCRC = r_packet[1] + r_packet[2] + r_packet[3] + r_packet[4] + r_packet[5];
@@ -263,6 +343,7 @@ void printPacketInfo(byte p_ver) {
     if(packet[3] & (1 << 2)) Serial.println("Тормоз '!'");
     Serial.printf("Ток: %d Ампер\n", packet[4]);
     Serial.printf("Скорость: %d км/ч\n", mySpeed);
+    displaySpeed(mySpeed);
   } else if (p_ver == 2) {
     mk_speed = (packet[5]<<8)+packet[6];
     mySpeed = ((((1000/mk_speed)*60) * ((WHEEL_D*3.14)/39)) * 60) / 1000;
@@ -278,22 +359,41 @@ void printPacketInfo(byte p_ver) {
 }
 
 uint8_t readACC() {
-  uint8_t i, acc_vol;
-  
+  uint8_t i, acc_vol, max_speed;
+
+  max_speed = r_packet[2] * 2;
   // read the input on analog pin ACC_PIN
   sensorValue = 0;
   for(i=0;i<100;i++) sensorValue += analogRead(ACC_PIN);
     
   sensorValue = (sensorValue/100);
-  if(sensorValue < 800) sensorValue = 800;
-  if(sensorValue > 3050) sensorValue = 3050;
-  acc_vol = map(sensorValue, 800, 3050, 0, 205);
+  if(sensorValue < 800) sensorValue = min_acc_value; // 800
+  if(sensorValue > 3050) sensorValue = max_acc_value; // 3050
+//  acc_vol = map(sensorValue, 800, 3050, 0, 205);
+  acc_vol = map(sensorValue, min_acc_value, max_acc_value, 0, max_speed);
   if(acc_vol < 10) acc_vol = 0;
 
   //voltage = (sensorValue/100) * (3.3 / 4095.0);
 
   return acc_vol;
 }
+
+uint8_t readREG() {
+  uint8_t i, reg_vol;
+  
+  // read the input on analog pin REG_PIN
+  sensorValue = 0;
+  for(i=0;i<19;i++) sensorValue += analogRead(REG_PIN);
+    
+  sensorValue = (sensorValue/20);
+  if(sensorValue < 800) sensorValue = min_reg_value; // 800
+  if(sensorValue > 3050) sensorValue = max_reg_value; // 3050
+  reg_vol = map(sensorValue, min_reg_value, max_reg_value, 0, 200);
+  if(reg_vol < 10) reg_vol = 0;
+
+  return reg_vol;
+}
+
 
 void calc_start_CRC(uint8_t p_ver) {
   int16_t myCRC;
@@ -307,4 +407,9 @@ void calc_start_CRC(uint8_t p_ver) {
     // TODO:
   }
   
+}
+
+void displaySpeed(uint8_t mk_speed) {
+  display.drawString(64, 22, String(mk_speed));
+  display.display();
 }
